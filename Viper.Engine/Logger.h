665d53b008bb22d4e644c73fce6e2d9ebda55943
Kallum <lgkscast@gmail.com>
2020-10-10 00:48:27 +0100

    Created logging system
    Initial stages of program started with logging system complete

---
+++ Viper.Engine/Logger.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace Viper {
+	class Logger {
+	public:
+		static void Trace(const char* message, ...);
+		static void Log(const char* message, ...);
+		static void Warn(const char* message, ...);
+		static void Error(const char* message, ...);
+		static void Fatal(const char* message, ...);
+	};
+}
